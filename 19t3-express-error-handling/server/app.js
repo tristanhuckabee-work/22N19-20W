f84bdb081dece112b0ee9a792aa310e800a98231
tristan <thuckabee.work@gmail.com>
2023-04-04 19:47:55 -0500

    setup

--- 19t3-express-error-handling/server/app.js
+++ 19t3-express-error-handling/server/app.js
@@ -5,19 +5,6 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
-app.use((req, res, next) => {
-  const error = new Error("Sorry, the requested resource couldn't be found");
-  error.statusCode = 404;
-  next(error);
-});
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.statusCode || 500);
-  res.send({
-    "message": err.message,
-    "statusCode": res.statusCode
-  });
-});
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));

7a58d2c3b9c7e62be8b3da876e0a717ddc980818
tristan <thuckabee.work@gmail.com>
2023-04-06 17:40:32 -0500

    setup for main push

---
+++ 19s-long-practice/server/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+require('express-async-errors');
+
+const app = express();
+
+app.use(express.json());
+app.use('/static', express.static('assets'));
+
+
+// For testing purposes, GET /
+app.get('/', (req, res) => {
+  res.json("Express server running. No content provided at root level. Please use another route.");
+});
+
+// For testing express.json middleware
+app.post('/test-json', (req, res, next) => {
+  // send the body as JSON with a Content-Type header of "application/json"
+  // finishes the response, res.end()
+  res.json(req.body);
+  next();
+});
+
+// For testing express-async-errors
+app.get('/test-error', async (req, res) => {
+  throw new Error("Hello World!")
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.send(`Error: ${err.message}`);
+})
+
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));

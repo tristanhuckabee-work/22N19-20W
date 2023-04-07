eac0b55036ff81c9e0cf029da27d86d488a2e5ce
tristan <thuckabee.work@gmail.com>
2023-04-06 19:45:26 -0500

    phase 2 done

--- 19s-long-practice/server/app.js
+++ 19s-long-practice/server/app.js
@@ -5,7 +5,12 @@ const app = express();
 
 app.use(express.json());
 app.use('/static', express.static('assets'));
-
+app.use('/', (req, res, next) => {
+  res.on('finish', () => {
+    console.log(`${req.method}${req.url} : ${res.statusCode}`);
+  });
+  next();
+});
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
@@ -22,13 +27,21 @@ app.post('/test-json', (req, res, next) => {
 
 // For testing express-async-errors
 app.get('/test-error', async (req, res) => {
-  throw new Error("Hello World!")
+  let error = new Error("Hello World!");
+  error.statusCode = 200;
+  throw error;
 });
 
+app.use((req, res, next) => {
+  let error = new Error('Resource Not Found');
+  error.statusCode = 404;
+  next(error);
+});
 app.use((err, req, res, next) => {
   console.log(err);
+  res.status(err.statusCode || 404);
   res.send(`Error: ${err.message}`);
-})
+});
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
